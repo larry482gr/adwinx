31e0dfb76ba18b753b2bf98b4c4b454c9c9b3d31
Lazaros Kazantzis <lakazantzis@gmail.com>
2015-11-25 23:51:22 +0200

    Updated styles.

--- public/assets/application-7d1c4220070f750f8fe299882ccdd33b5ff6128e4f14043896d8cae17758c7f4.js
+++ public/assets/application-7d1c4220070f750f8fe299882ccdd33b5ff6128e4f14043896d8cae17758c7f4.js
@@ -16417,9 +16417,8 @@ $(document).ready(function() {
     }
 });
 // Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
-
 $(document).ready(function(){
-    var previewNode = document.querySelector("#template");
+    var previewNode = document.getElementById("template");
     previewNode.id = "";
     var previewTemplate = previewNode.parentNode.innerHTML;
     previewNode.parentNode.removeChild(previewNode);
@@ -16432,38 +16431,38 @@ $(document).ready(function(){
         previewTemplate: previewTemplate,
         autoQueue: false, // Make sure the files aren't queued until manually added
         previewsContainer: "#previews", // Define the container to display the previews
-        clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+
     });
 
     myDropzone.on("addedfile", function(file) {
         // Hookup the start button
-        file.previewElement.querySelector(".start").onclick = function() { myDropzone.enqueueFile(file); };
+        file.previewElement.getElementsByClassName("start").onclick = function() { myDropzone.enqueueFile(file); };
     });
 
 // Update the total progress bar
     myDropzone.on("totaluploadprogress", function(progress) {
-        document.querySelector("#total-progress .progress-bar").style.width = progress + "%";
+        document.getElementsByClassName("progress-bar").style.width = progress + "%";
     });
 
     myDropzone.on("sending", function(file) {
         // Show the total progress bar when upload starts
-        document.querySelector("#total-progress").style.opacity = "1";
+        document.getElementById("#total-progress").style.opacity = "1";
         // And disable the start button
-        file.previewElement.querySelector(".start").setAttribute("disabled", "disabled");
+        file.previewElement.getElementsByClassName("start").setAttribute("disabled", "disabled");
     });
 
 // Hide the total progress bar when nothing's uploading anymore
     myDropzone.on("queuecomplete", function(progress) {
-        document.querySelector("#total-progress").style.opacity = "0";
+        document.getElementById("total-progress").style.opacity = "0";
     });
 
 // Setup the buttons for all transfers
 // The "add files" button doesn't need to be setup because the config
 // `clickable` has already been specified.
-    document.querySelector("#actions .start").onclick = function() {
+    document.getElementsByClassName("start").onclick = function() {
         myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
     };
-    document.querySelector("#actions .cancel").onclick = function() {
+    document.getElementsByClassName("cancel").onclick = function() {
         myDropzone.removeAllFiles(true);
     };
 });
