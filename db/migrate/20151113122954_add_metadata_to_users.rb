f715e30bdae51456974c59530941a3a3c879fa48
Lazaros Kazantzis <lakazantzis@gmail.com>
2015-12-10 04:13:27 +0200

    Added default value in users metadata field.

--- db/migrate/20151113122954_add_metadata_to_users.rb
+++ db/migrate/20151113122954_add_metadata_to_users.rb
@@ -1,5 +1,5 @@
 class AddMetadataToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :metadata, :text, null: false
+    add_column :users, :metadata, :text, null: false, default: '[first_name, last_name]'
   end
 end
