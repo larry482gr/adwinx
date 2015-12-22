cd33173b7e7b5555f877aac8e747313c95d74a66
Lazaros Kazantzis <lakazantzis@gmail.com>
2015-12-22 16:37:30 +0200

    Set utf8 collation in sms campaigns label and originator fields.

--- db/migrate/20151214103210_create_sms_campaigns.rb
+++ db/migrate/20151214103210_create_sms_campaigns.rb
@@ -23,9 +23,6 @@ class CreateSmsCampaigns < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    change_column :sms_campaigns, :originator, 'VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci'
-    change_column :sms_campaigns, :label, 'VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci'
-
     add_index :sms_campaigns, :user_id, name: 'idx_sms_campaigns_user_id'
     add_foreign_key :sms_campaigns, :users, column: :user_id, primary_key: :id, on_update: :cascade
 
